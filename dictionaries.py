3318aa0a8907a30453474e6f8d2186e62845a427
Laura Castro <lauracastrotech@gmail.com>
2025-02-13 11:42:07 -0500

    complete practice problem

--- dictionaries.py
+++ dictionaries.py
@@ -1,3 +1,12 @@
 # February 13, 2025
 # Programmer: Laura Castro
 # Practice problems
+
+def get_value_from_dictionary(dict, key):
+    for value in dict:
+        if value == key:
+            return dict[key]  
+    return None
+
+
+
